f66e1e2c1637d577a49cf7525d5a7083dc399ee4
Asif-Iqbal-HSTU <asif.iqbal.hstu@gmail.com>
2024-09-25 18:23:57 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [things, setThings] = useState([]);
+    const [name, setName] = useState('');
+
+    useEffect(() => {
+        axios.get('http://10.0.2.2:8000/api/things')
+            .then(response => {
+                setThings(response.data);
+            })
+            .catch(error => console.error(error));
+    }, []);
+
+    const addThing = (e) => {
+        e.preventDefault();
+        axios.post('http://10.0.2.2:8000/api/things', { name })
+            .then(response => {
+                setThings([...things, response.data]);
+                setName('');
+            })
+            .catch(error => console.error(error));
+    };
+
+    return (
+        <div>
+            <h1>Things List</h1>
+            <form onSubmit={addThing}>
+                <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder="Add a new thing"
+                />
+                <button type="submit">Add</button>
+            </form>
+            <ul>
+                {things.map(thing => (
+                    <li key={thing.id}>{thing.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
